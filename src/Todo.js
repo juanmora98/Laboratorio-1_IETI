7dff1d28b352951d323f9fd39ef25ef0adcf418b
juanmora98 <juan.mora-q@mail.escuelaing.edu.co>
2020-08-19 10:16:14 -0500

    Commit inicial y parte inicial punto 2
    Se creo la base inicial del trabajo y se realizo los 3 primeros puntos de la parte 2

---
+++ src/Todo.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export class Todo extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }   
+
+    render() {
+        return (  
+            <div>
+                <h2>{this.props.text}</h2>
+                <h2>{this.props.priority}</h2>
+                <h2>{this.props.dueDate}</h2>
+            </div>
+        );
+    }
+
+}
